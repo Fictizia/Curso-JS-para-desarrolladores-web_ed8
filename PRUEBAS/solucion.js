927613e99f78084d8842ef8780342dd2b892c045
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-07-04 17:52:52 +0000

    Analisis de la solución y requisitos. #8

---
+++ PRUEBAS/solucion.js
@@ -0,0 +1,32 @@
+/* ----- REQUISITOS -----
+
+**Características:**
+
+- [] Cada asistente recibirá el identificador de su posición en el Array.
+- [] El identificador no puede cambiar una vez reservado el asiento.
+- [] Los asistentes pueden darse de baja, y el asiento que ocupaban queda libre.
+- [] Los nuevos asistentes seran asigandos primero a asientos desocupados.
+
+
+**Opcional:**
+
+- [] Verifica antes de asignar un asistente que no se registro previamente.
+
+*/
+
+/* ----- SOLUCIÓN ----- */
+
+// Lista clientes
+
+/* 
+funcion añadir
+	- añadir en hueco o añadir al final
+	- (opt) ver si existe
+*/
+
+
+/*
+ función para quitar...
+	- cambiar a undefined
+	- 
+*/

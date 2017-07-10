9e3943500fd55940cb06e7a382bbbe5e00696d0d
Juan Cano <jac.arnaiz@gmail.com>
2017-07-10 11:00:19 +0200

    Ordenando ejercicios

---
+++ JuanCano/Clase 4/Ejercicio 3.js
@@ -0,0 +1,6 @@
+console.time("presentacion");
+console.log('%c Juan ', 'background: #80ba27; color: #00FFF; font-size:54px;');
+console.info('%c Juan ', 'background: #3EBDFF; color: #FFF; font-size:25px;');
+console.warn('%c Juan ', 'background: #FFF06A; color: #FFF; font-size:25px;');
+console.error('%c Juan ', 'background: #FF1000; color: #FFF; font-size:25px;');
+console.timeEnd("presentacion");
---
+++ JuanCano/Clase 4/Ejercicio 5.js
@@ -0,0 +1,8 @@
+function operacionesMatematicas(numero1, numero2){
+	console.info("El resultado de la suma es:", numero1+numero2);
+	console.info("El resultado de la resta es:",numero1-numero2);
+	console.info("El resultado de la división es:", numero1/numero2);
+	console.info("El resultado de la división es:", numero1*numero2)
+};
+
+operacionesMatematicas(33,82);
